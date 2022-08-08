2c9b2b0729dcf199273bbb79db0aa6d120a1abf8
little-boy52 <1144612318@qq.com>
2022-08-09 00:12:44 +0800

    :sparkles: Add memory_pool.h

---
+++ src/memory_pool.h
@@ -0,0 +1,76 @@
+#ifndef MEMORY_POOL_H_
+#define MEMORY_POOL_H_
+
+#include <cstdint> // uintptr_t
+#include <cstddef> // size_t
+#include <new> // placement new
+#include <utility> // std::swap
+
+template <typename T, size_t BlockSize = 4096>
+class MemoryPool {
+public:
+    // 定义基本类型
+    typedef T               value_type;
+    typedef T*              pointer;
+    typedef T&              reference;
+    typedef const T*        const_pointer;
+    typedef const T&        const_reference;
+    typedef size_t          size_type;
+    typedef ptrdiff_t       difference_type;
+
+    // todo
+    template <typename U>
+    struct rebind {
+        typedef MemoryPool<U> other;
+    };
+
+    MemoryPool() noexcept;
+    // todo
+    MemoryPool(const MemoryPool &rhs) noexcept;
+    MemoryPool(MemoryPool &&rhs) noexcept;
+
+    ~MemoryPool() noexcept;
+
+    MemoryPool& operator=(const MemoryPool &rhs) = delete;
+    MemoryPool& operator=(MemoryPool &&rhs) noexcept;
+
+    pointer address(reference x) const noexcept;
+    const_pointer address(const_reference x) const noexcept;
+    size_type max_size() const noexcept;
+
+    pointer allocate(size_type n = 1, const_pointer hint = 0);
+    void deallocate(pointer p, size_type n = 1);
+    template <typename U, typename... Args>
+    void construct(U* p, Args&&... args);
+
+    template <typename U>
+    void destroy(U* p);
+
+    template <typename... Args>
+    pointer newElement(Args&&... args);
+    void deleteElement(pointer p);
+
+private:
+    union Slot_ {
+        value_type element;
+        Slot_* next;
+    };
+
+    typedef char*               data_pointer_;
+    typedef Slot_               slot_type_;
+    typedef Slot_*              slot_pointer_;
+
+    slot_pointer_ currentBlock_;
+    slot_pointer_ currentSlot_;
+    slot_pointer_ lastSlot_;
+    slot_pointer_ freeSlots_;
+
+    size_type padPointer(data_pointer_ p, size_type align) const noexcept;
+    void allocateBlock();
+
+    static_assert(BlockSize >= 2*sizeof(slot_type_), "BlockSize too small.");
+};
+
+#include "memory_pool.tcc"
+
+#endif // !MEMORY_POOL_H_
